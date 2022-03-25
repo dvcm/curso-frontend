c4d2cec90a307580ed8f53acd0a891e9374ef097
Patrick Passarella <pshaggy@gmail.com>
2022-03-25 00:16:16 -0300

    commit

--- Modulo-18-Gulp/gulpfile.js
+++ Modulo-18-Gulp/gulpfile.js
@@ -3,7 +3,7 @@ const concat = require('gulp-concat')
 const cssmin = require('gulp-cssmin')
 const rename = require('gulp-rename')
 const uglify = require('gulp-uglify')
-const image = require('gulp-imagemin');
+const image = require('gulp-image');
 const stripJs = require('gulp-strip-comments')
 const stripCss = require('gulp-strip-css-comments')
 
